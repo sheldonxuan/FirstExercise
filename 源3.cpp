ed12127efe9521feb8cde9fb73740046c46240a0
Sheldon <601911447@qq.com>
2019-09-15 16:54:04 +0800

    加入可输入的高斯卷积核n

--- 源3.cpp
+++ 源3.cpp
@@ -1,11 +1,15 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
 using namespace cv;
+using namespace std;
 int main(int, char**)
 {
+	int n;
 	VideoCapture cap(0); // open the default camera
 	if (!cap.isOpened())  // check if we succeeded
 		return -1;
-
+	cout << "请输入高斯模糊卷积核大小：" << endl;
+	cin >> n;
 	Mat edges;
 	namedWindow("edges", 1);
 	for (;;)
@@ -13,7 +17,7 @@ int main(int, char**)
 		Mat frame;
 		cap >> frame; // get a new frame from camera
 		cvtColor(frame, edges, CV_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		GaussianBlur(edges, edges, Size(2*n-1, 2*n-1), 1.5, 1.5);
 		Canny(edges, edges, 0, 30, 3);
 		imshow("edges", edges);
 		if (waitKey(30) >= 0) break;
